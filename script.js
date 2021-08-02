3f7ebfc447919ef011c1e38dfb75d880986b3a31
divokaka32 <divo.kaka32@gmail.com>
2021-08-02 22:03:48 +0700

    perubahan isi dari index.html dan script.js (async)

--- script.js
+++ script.js
@@ -1,69 +1,125 @@
-$('.search-button').on('click', function () {
-    $.ajax({
-        url: 'http://www.omdbapi.com/?apikey=906cdda&s=' + $('.input-keyword').val(),
-        success: result => {
-            const movieDetail = result.Search;
-            // console.log(movieDetail);
-            // Card
-            let cards = '';
-            movieDetail.forEach(m => {
-                cards += showCards(m);
-            });
-            $('.movie-container').html(cards);
-            // Modal
-            $('.modal-detail-button').on('click', function () {
-                $.ajax({
-                    url: 'http://www.omdbapi.com/?apikey=906cdda&i=' + $(this).data('imdbid'),
-                    success: m => {
-                        const movieDetail = showMovieDetail(m);
-                        $('.modal-body').html(movieDetail);
-                    },
-                    error: e => {
-                        console.log(e.responseText)
-                    }
-                })
-            })
-        },
-        error: e => {
-            console.log(e.responseText);
-        }
+// ketika tombol di ketik
+const searchButton = document.querySelector('.search-button');
+searchButton.addEventListener('click', async function () {
+    try {
+        const inputKeyword = document.querySelector('.input-keyword');
+        const movies = await getMovie(inputKeyword.value);
+        // console.log(movie)
+        updateUI(movies);
+    } catch (err) {
+        alert(err);
+    }
+});
+
+function getMovie(inputKeyword) {
+    return fetch('http://www.omdbapi.com/?apikey=906cdda&s=' + inputKeyword)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(response.statusText);
+            }
+            return response.json();
+        })
+        .then(response => {
+            if (response.Response === "False") {
+                throw new Error(response.Error);
+            }
+            // return console.log(response);
+            return response.Search;
+        })
+};
+
+function updateUI(movies) {
+    let card = '';
+    // movies di looping untuk mengambil setiap dari movie yang tersedia
+    movies.forEach(m => {
+        // movie ditampung ke dalam card
+        card += showCard(m)
     });
+    // tangkap baris untuk menampung card movies
+    const movieContainer = document.querySelector('.movie-container');
+    // card di taruh/cetak ke dalam html
+    movieContainer.innerHTML = card;
+}
 
-})
-
-
-function showCards(m) {
-    return `<div class="col-md-4 my-3">
+function showCard(m) {
+    return `<div class="col-md-4 mt-3">
                 <div class="card" style="width: 18rem">
-                <img src="${m.Poster}" class="card-img-top" alt="..." />
+                <img src="${m.Poster}" class="card-img-top" />
                 <div class="card-body">
                     <h5 class="card-title">${m.Title}</h5>
                     <h6 class="card-subtitle mb-2 text-muted">${m.Year}</h6>
-                    <a href="#" class="btn btn-primary modal-detail-button" data-bs-toggle="modal" data-bs-target="#movieDetail" data-imdbid="${m.imdbID}">Detail</a>
+                    <a href="#" class="btn btn-primary detail-button" data-bs-toggle="modal" data-bs-target="#MovieDetail" data-imdbid="${m.imdbID}">Detail</a>
+                </div>
+                </div>
+            </div>`
+};
+
+
+// event binding
+// ketika klik detail di tekan
+document.addEventListener('click', async function (e) {
+    if (e.target.classList.contains('detail-button')) {
+        // console.log('ok');
+        try {
+
+            const imdbid = e.target.dataset.imdbid;
+            const movieDetail = await getMovieDetail(imdbid);
+            // console.log(movieDetail);
+            updateUIDetail(movieDetail);
+        } catch (err) {
+            console.log(err)
+        }
+    }
+});
+
+function getMovieDetail(imdbid) {
+    return fetch('http://www.omdbapi.com/?apikey=906cdda&i=' + imdbid)
+        .then(response => {
+            // console.log(response)
+            if (!response.ok) {
+                throw new Error(response.statusText);
+            }
+            return response.json();
+        })
+        .then(response => response)
+}
+
+function updateUIDetail(md) {
+    let movieDetail = showMovieDetail(md);
+    // tangkap modal body
+    const modalBody = document.querySelector('.modal-body');
+    // isi di masukkan
+    modalBody.innerHTML = movieDetail;
+}
+
+
+function showMovieDetail(md) {
+    return `<div class="container">
+                <div class="row">
+                <div class="col-md-3">
+                    <img src="${md.Poster}" class="img-fluid" />
+                </div>
+                <div class="col-md">
+                    <ul class="list-group">
+                    <li class="list-group-item"><strong>Genre : </strong>${md.Genre}</li>
+                    <li class="list-group-item"><strong>Director : </strong>${md.Director}</li>
+                    <li class="list-group-item"><strong>Actor : </strong>${md.Actors}</li>
+                    <li class="list-group-item"><strong>Rating IMDb : </strong>${md.Ratings[0].Value}</li>
+                    <li class="list-group-item"><strong>Plot : </strong>${md.Plot}</li>
+                    </ul>
                 </div>
                 </div>
             </div>`
 }
 
-function showMovieDetail(a) {
-    return `<div class="container">
-                <div class="row">
-                <div class="col-md-3">
-                    <img src="${a.Poster}" class="img-fluid" />
-                </div>
-                <div class="col-md">
-                    <ul class="list-group">
-                    <li class="list-group-item">
-                        <h4>${a.Title}</h4>
-                        (${a.Year})
-                    </li>
-                    <li class="list-group-item"><strong>Genre : </strong>${a.Genre}</li>
-                    <li class="list-group-item"><strong>Directors : </strong>${a.Director}</li>
-                    <li class="list-group-item"><strong>Actors : </strong>${a.Actors}</li>
-                    <li class="list-group-item"><strong>Rating IMDb : </strong>${a.Ratings[0].Value}</li>
-                    <li class="list-group-item"><strong>Plot : </strong>${a.Plot}</li>
-                    </ul>
-                </div>
-                </div>
-            </div>`
+function alert(err) {
+    const alertPopup = document.querySelector('.alert-popup');
+    const alert = `<div class="alert alert-danger d-flex align-items-center" role="alert">
+                        <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Danger:"><use xlink:href="#exclamation-triangle-fill"/></svg>
+                        <div>
+                        ${err}
+                        </div>
+                    </div>`;
+    return alertPopup.innerHTML = alert;
+
 }
